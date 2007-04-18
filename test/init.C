3bb519d920cc32284ee79af18b75ecac7500b4c5
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2007-04-18 20:04:08 +0000

    cleaned up test cfg files

--- test/init.C
+++ test/init.C
@@ -1,7 +1,6 @@
 {
 
 gSystem->Load("libFWCoreFWLite.so");
-gSystem->Load("libRecoParticleFlowPFBlockProducer.so");
 AutoLibraryLoader::enable();
 gSystem->Load("libCintex.so");
 ROOT::Cintex::Cintex::Enable();
